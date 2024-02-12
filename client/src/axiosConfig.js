78107c8f0a207b4da614d6cbd1af6558642f25f8
Navi <navisenthilnadhan@gmail.com>
2024-02-12 07:05:51 -0800

    Changed the backend url

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 // Set up the default base URL for Axios
-axios.defaults.baseURL = 'http://localhost:5000'; // Adjust the base URL as needed
+axios.defaults.baseURL = 'https://studentbackend-t5ld.onrender.com'; // Adjust the base URL as needed
 
 export default axios;
